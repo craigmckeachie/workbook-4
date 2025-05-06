c658fa8b7654c5fbaaee21e4a21a1c950de45e95
Craig McKeachie <craigdmckeachie@gmail.com>
2025-05-06 10:38:02 -0400

    Room pape 32 completed

--- HotelOperations/src/main/java/com/pluralsight/Main.java
+++ HotelOperations/src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,15 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        Room room112 = new Room(1,189, true, true);
+        System.out.println(room112.isAvailable());
+
+        Room room113 = new Room(1,189, false, true);
+        System.out.println(room113.isAvailable());
+
+        Room room114 = new Room(1,189, false, false);
+        System.out.println(room114.isAvailable());
+
     }
 }
