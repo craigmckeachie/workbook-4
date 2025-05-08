d7d104a0ef59bad0511e52608e6f0373822970ff
Craig McKeachie <craigdmckeachie@gmail.com>
2025-05-08 14:09:28 -0400

    other models created

--- BlackJack/src/main/java/com/mgm/blackjack/models/Card.java
+++ BlackJack/src/main/java/com/mgm/blackjack/models/Card.java
@@ -4,11 +4,13 @@ public class Card {
     private String suit;
     private String value;
     private boolean isFaceUp;
+
     public Card(String suit, String value) {
         this.suit = suit;
         this.value = value;
         this.isFaceUp = false;
     }
+
     public String getSuit(){
         // only return the suit if the card is face up
         if(isFaceUp){
@@ -17,6 +19,7 @@ public class Card {
             return "#";
         }
     }
+
     public String getValue(){
         // only return the value if the card is face up
         if(isFaceUp){
@@ -27,6 +30,7 @@ public class Card {
             return "#";
         }
     }
+
     public int getPointValue(){
         // only return the value if the card is face up
         if(isFaceUp){
@@ -39,9 +43,11 @@ public class Card {
             return -1;
         }
     }
+
     public boolean isFaceUp(){
         return isFaceUp;
     }
+
     public void flip(){
         isFaceUp = !isFaceUp;
     }
